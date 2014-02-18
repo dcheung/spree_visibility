e0c12c7b001a00ec024e11f8de93778062c99079
Steven Barragan <me@steven.mx>
2014-02-18 11:21:46 -0600

    No display product if you're in a hiden taxon page

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,7 +1,11 @@
 Spree::Product.class_eval do
   attr_accessible :visible
 
-  def self.visible
+  add_search_scope :visible do
     where(visible: true)
+  end
+
+  add_search_scope :none do
+    where(:id => nil).where("#{ self.table_name }.id IS NOT ?", nil)
   end
 end
--- app/models/search/base_decorator.rb
+++ app/models/search/base_decorator.rb
@@ -2,6 +2,8 @@ Spree::Core::Search::Base.class_eval do
 
   protected
   durably_decorate :get_base_scope, mode: 'soft', sha: '0030f517d06459bfcde9ae18157806c3b702a71f' do
-    original_get_base_scope.visible
+    base_scope = original_get_base_scope.visible
+
+    taxon && !taxon.visible ? base_scope.none : base_scope
   end
 end
