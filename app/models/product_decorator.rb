e537fd6d8cb25aec31bd3aa9bd8ea6c2f93771d2
Dan Cheung <dcheung@users.noreply.github.com>
2015-08-14 05:27:19 -0700

    Update product_decorator.rb

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,5 +1,4 @@
 Spree::Product.class_eval do
-  attr_accessible :visible
 
   add_search_scope :visible do
     where(visible: true)
@@ -8,4 +7,12 @@ Spree::Product.class_eval do
   add_search_scope :none do
     where(:id => nil).where("#{ self.table_name }.id IS NOT ?", nil)
   end
+  
+  private
+    # Using a private method to encapsulate the permissible parameters is just a good pattern
+    # since you'll be able to reuse the same permit list between create and update. Also, you
+    # can specialize this method with per-user checking of permissible attributes.
+    def product_params
+      params.permit(:visible)
+    end  
 end
