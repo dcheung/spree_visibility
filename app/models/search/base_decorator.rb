a48ce01b1bff00e74b02a22ad5f1ca4ed34ac512
Dan Cheung <dcheung@users.noreply.github.com>
2015-08-14 05:43:57 -0700

    Update base_decorator.rb

--- app/models/search/base_decorator.rb
+++ app/models/search/base_decorator.rb
@@ -1,7 +1,9 @@
+require 'durable_decorator_rails'
+
 Spree::Core::Search::Base.class_eval do
 
   protected
-  durably_decorate :get_base_scope, mode: 'strict' do |text|
+  durably_decorate :get_base_scope, mode: 'soft', sha: '0030f517d06459bfcde9ae18157806c3b702a71f' do
     base_scope = original_get_base_scope.visible
 
     taxon && !taxon.visible ? base_scope.none : base_scope
