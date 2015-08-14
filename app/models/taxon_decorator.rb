dfc234777a0c34d4a975728cdf2aaec4f0abb8a2
Dan Cheung <dcheung@users.noreply.github.com>
2015-08-14 05:46:44 -0700

    Update taxon_decorator.rb

--- app/models/taxon_decorator.rb
+++ app/models/taxon_decorator.rb
@@ -1,7 +1,13 @@
 Spree::Taxon.class_eval do
-  attr_accessible :visible
-
   def self.visible
     where(visible: true)
   end
+  
+  private
+    # Using a private method to encapsulate the permissible parameters is just a good pattern
+    # since you'll be able to reuse the same permit list between create and update. Also, you
+    # can specialize this method with per-user checking of permissible attributes.
+    def taxon_params
+      params.permit(:visible)
+    end    
 end
